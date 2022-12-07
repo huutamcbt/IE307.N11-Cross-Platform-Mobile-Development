fcf3ddeeb73a0547c2062907c7330c56c62a32e0
WindyWind <19521362@gm.uit.edu.vn>
2022-12-07 15:20:37 +0700

    model

---
+++ Frontend/Frontend/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Frontend.Models
+{
+    public class Product
+    {
+        private int ID { get; set; }
+        private string Name { get; set; }
+        private double Price { get; set; }
+        private string Image { get; set; }
+        private int CategoryID { get; set; }
+        private int Stock { get; set; }
+        private int Quantity { get; set; } // Chi frontend co
+        private string Description { get; set; }
+
+
+
+    }
+}
