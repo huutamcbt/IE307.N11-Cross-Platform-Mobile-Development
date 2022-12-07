7c8cf84f662265a1c993a08e8503b20f8f3745cc
WindyWind <19521362@gm.uit.edu.vn>
2022-12-07 15:20:56 +0700

    order model

---
+++ Frontend/Frontend/Models/OrderItems.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Frontend.Models
+{
+    public class OrderItems
+    {
+        private int ID { get; set; }
+        private int OrderID { get; set; }
+        private int ProductID { get; set; }
+        private int Quantity { get; set; }
+
+    }
+}
