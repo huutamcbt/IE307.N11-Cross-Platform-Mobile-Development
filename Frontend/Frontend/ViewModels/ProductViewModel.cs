19baed5277faa3486bb8bcea2778e238004bfa0a
WindyWind <19521362@gm.uit.edu.vn>
2022-12-24 15:59:20 +0700

    filterand sort

--- Frontend/Frontend/ViewModels/ProductViewModel.cs
+++ Frontend/Frontend/ViewModels/ProductViewModel.cs
@@ -10,46 +10,126 @@ using Frontend.Models;
 using Frontend.Views;
 using Xamarin.Forms;
 using Frontend.Services;
+using System.Linq;
 
 namespace Frontend.ViewModels
 {
     class ProductViewModel : INotifyPropertyChanged
     {
-        readonly IList<Product> source;
+        private bool loaded = false;
+        private List<Product> source;
+        private List<Category> source1;
+        public String title { get; private set; }
         public ObservableCollection<Product> productList { get; private set; }
+        public ObservableCollection<Category> categoryList { get; private set; }
         public ICommand ProductTapCommand { get; set; }
+        public ICommand CatTapCommand { get; set; }
+        public int sortMode = 0;
+        private int categoryID { get; set; }
 
+        public int SortMode
+        {
+            get => sortMode;
+            set {
+                sortMode = value;
+                if(loaded)
+                    OnPropertyChanged("sortMode");
+            }
+        }
         public ProductViewModel()
         {
+            title = "Tất cả";
             source = new List<Product>();
+            source1 = new List<Category>();
             CreateItemCollection();
-
+            InitializeCategoryCollection();
             ProductTapCommand = new Command<Product>(async (item) =>
             {
                 await Shell.Current.GoToAsync($"{nameof(ProductDetailPage)}?productID={item.ProductId}");
             });
+            CatTapCommand = new Command<Category>((item) =>
+            {
+                title = item.Name;
+                categoryID = item.ID;
+                CategoryFilterHandler();
+                OnPropertyChanged("title");
+            });
         }
 
+        void CategoryFilterHandler()
+        {
+            if (categoryID == 0)
+            {
+                SortModeChangeHandler(new List<Product>(source));
+                return;
+            }
 
+            List<Product> products = new List<Product>(source);
+            SortModeChangeHandler(products.FindAll((e) => e.CategoryID == categoryID));
+        }
+        void SortModeChangeHandler(List<Product> products )
+        {
+            switch (sortMode)
+            {
+                case 1: // asc name
+                    products.Sort((a, b) => String.Compare(a.Name, b.Name));
+                    break;
+                case 2: //desc name 
+                    products.Sort((a, b) => String.Compare(b.Name, a.Name));
+                    break;
+                case 3: //asc price
+                    products.Sort((a, b) => Convert.ToInt32(a.Price - b.Price));
+                    break;
+                case 4: //desc price
+                    products.Sort((a, b) => Convert.ToInt32(b.Price - a.Price));
+                    
+                    break;
+                case 0: //default
+                    break;
+                default:
+                    break;
+            }
+            productList = new ObservableCollection<Product>(products);
+            OnPropertyChanged("productList");
+        }
 
         async void CreateItemCollection()
         {
             ProductService productService = new ProductService();
             List<Product> products = await productService.GetAllProduct();
+
             foreach (Product product in products)
             {
                 source.Add(product);
             }
             productList = new ObservableCollection<Product>(source);
             OnPropertyChanged("productList");
+            loaded = true;
         }
 
+        async void InitializeCategoryCollection()
+        {
+            CategoriesService categoriesService = new CategoriesService();
+            List<Category> categories = await categoriesService.GetAllCategory();
+            source1.Add(new Category { ID = 0, Name = "Tất cả", Image = "star.png" });
+            foreach (Category category in categories)
+            {
+                source1.Add(category);
+            }
+            categoryList = new ObservableCollection<Category>(source1);
+            OnPropertyChanged("categoryList");
+        }
 
         #region INotifyPropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
 
+
         void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
+            if (propertyName == "sortMode")
+            {
+                SortModeChangeHandler(productList.ToList());
+            }
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
         #endregion
