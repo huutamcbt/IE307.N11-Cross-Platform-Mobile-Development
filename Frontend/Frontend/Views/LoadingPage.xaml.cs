541753b485e2f7ab89c57d91d02cdaf00c8cfee8
huutamcbt <huutamcbt@gmail.com>
2022-12-27 11:23:21 +0700

    Add Loading Page

---
+++ Frontend/Frontend/Views/LoadingPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Frontend.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoadingPage : ContentPage
+    {
+        public LoadingPage()
+        {
+            InitializeComponent();
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            
+            loading_progress.Progress = 0;
+            
+            await loading_progress.ProgressTo(1, 5000, Easing.SpringIn);
+            await Shell.Current.GoToAsync("//Main");
+        }
+
+    }
+}
