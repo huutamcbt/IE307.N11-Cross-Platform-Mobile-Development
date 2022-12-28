09bf807c47cda8dbd7457eaa3cbc63b95c717529
huutamcbt <huutamcbt@gmail.com>
2022-12-28 12:17:11 +0700

    Update Routing Register

--- Frontend/Frontend/AppShell.xaml.cs
+++ Frontend/Frontend/AppShell.xaml.cs
@@ -25,7 +25,7 @@ namespace Frontend
             Routing.RegisterRoute(nameof(CartPage), typeof(CartPage));
             Routing.RegisterRoute(nameof(RegisterPage), typeof(RegisterPage));
             Routing.RegisterRoute(nameof(LoginPage), typeof(LoginPage));
-          
+            Routing.RegisterRoute(nameof(TestPage), typeof(TestPage));
         }
 
     }
