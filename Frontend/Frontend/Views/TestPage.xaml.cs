b2bce4bd299b368afd88a66daa6ef1ac34a4f71d
huutamcbt <huutamcbt@gmail.com>
2022-12-28 10:36:48 +0700

    Add Test Page for test api

---
+++ Frontend/Frontend/Views/TestPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Frontend.Models;
+using Newtonsoft.Json;
+
+namespace Frontend.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TestPage : ContentPage
+    {
+        public TestPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void send_request_Clicked(object sender, EventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync("http://foodbookingapi.somee.com/api/GetProductById/1");
+            string productList = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(productList);
+            Product list = JsonConvert.DeserializeObject<Product>(productList);
+            var statusCode = response.StatusCode;
+            response_content.Text = $"Status code: {statusCode}\n"
+                                    + $"Content: {list.Name}";
+        }
+    }
+}
