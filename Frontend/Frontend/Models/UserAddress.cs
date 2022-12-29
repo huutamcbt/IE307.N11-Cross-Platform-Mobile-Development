558323bd48b7da62b261771a0f6849e6d8f5c090
WindyWind <19521362@gm.uit.edu.vn>
2022-12-29 09:02:39 +0700

    service

--- Frontend/Frontend/Models/UserAddress.cs
+++ Frontend/Frontend/Models/UserAddress.cs
@@ -13,7 +13,6 @@ namespace Frontend.Models
         public string Province { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
-        public bool isDefault { get; set; }
-
+        public string Mobile { get; set; }
     }
 }
