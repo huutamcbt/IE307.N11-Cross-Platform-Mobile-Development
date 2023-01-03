dd6141660b719a7472354a418455be4343f67f21
huutamcbt <huutamcbt@gmail.com>
2023-01-03 08:16:31 +0700

    Add some controllers

---
+++ Backend/FoodBookingAPI/FoodBookingAPI/Controllers/ReviewController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using FoodBookingAPI.Models;
+using FoodBookingAPI.Repository;
+using Newtonsoft.Json;
+
+namespace FoodBookingAPI.Controllers
+{
+    public class ReviewController : ApiController
+    {
+        Dictionary<string, object> param;
+
+        [Route("api/GetReviewsByProductId/{ProductId}")]
+        [HttpGet]
+        public IHttpActionResult GetReviewsByProductId(int ProductId)
+        {
+            try
+            {
+                param = null;
+                param = new Dictionary<string, object>();
+
+                param.Add(nameof(Reviews.ProductId), ProductId);
+
+                DataTable result = ReviewRepository.GetReviewsByProductId(param);
+
+                if (result != null)
+                    return Ok(result);
+                return NotFound();
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+        }
+
+        [Route("api/AddReview")]
+        [HttpPost]
+        public IHttpActionResult AddReview()
+        {
+            try
+            {
+                param = null;
+                param = new Dictionary<string, object>();
+
+                string content = Request.Content.ReadAsStringAsync().Result;
+
+                string jsonContent = "[" + content + "]";
+
+                Reviews newReview = JsonConvert.DeserializeObject<List<Reviews>>(jsonContent)[0];
+
+                param.Add(nameof(Reviews.ProductId), newReview.ProductId);
+                param.Add(nameof(Reviews.Rating), newReview.Rating);
+                param.Add(nameof(Reviews.UserId), newReview.UserId);
+                param.Add(nameof(Reviews.Content), newReview.Content);
+                param.Add(nameof(Reviews.CreatedDate), newReview.CreatedDate);
+                param.Add(nameof(Reviews.ModifiedDate), newReview.ModifiedDate);
+                param.Add(nameof(Reviews.DeletedDate), newReview.DeletedDate);
+
+                bool success = ReviewRepository.AddReview(param);
+                if (success == true)
+                    return Ok();
+                return NotFound();
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+        }
+
+        [Route("api/UpdateReview")]
+        [HttpPost]
+        public IHttpActionResult UpdateReview()
+        {
+            try
+            {
+                param = null;
+                param = new Dictionary<string, object>();
+
+                string content = Request.Content.ReadAsStringAsync().Result;
+                string jsonContent = "[" + content + "]";
+                Reviews updateReview = JsonConvert.DeserializeObject<List<Reviews>>(jsonContent)[0];
+
+                param.Add(nameof(Reviews.ReviewId), updateReview.ReviewId);
+                param.Add(nameof(Reviews.ProductId), updateReview.ProductId);
+                param.Add(nameof(Reviews.Rating), updateReview.Rating);
+                param.Add(nameof(Reviews.UserId), updateReview.UserId);
+                param.Add(nameof(Reviews.Content), updateReview.Content);
+                param.Add(nameof(Reviews.CreatedDate), updateReview.CreatedDate);
+                param.Add(nameof(Reviews.ModifiedDate), updateReview.ModifiedDate);
+                param.Add(nameof(Reviews.DeletedDate), updateReview.DeletedDate);
+
+                bool success = ReviewRepository.UpdateReview(param);
+                if (success == true)
+                    return Ok();
+                return NotFound();
+
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+        }
+
+        [Route("api/DeleteReview/{ReviewId}")]
+        [HttpDelete]
+        public IHttpActionResult DeleteReview(int ReviewId)
+        {
+            try
+            {
+                param = null;
+                param = new Dictionary<string, object>();
+
+                param.Add(nameof(Reviews.ReviewId), ReviewId);
+
+                bool success = ReviewRepository.DeleteReview(param);
+
+                if (success == true)
+                    return Ok();
+                return NotFound();
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ Backend/FoodBookingAPI/FoodBookingAPI/Controllers/UserController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using FoodBookingAPI.Models;
+using FoodBookingAPI.Repository;
+using Newtonsoft.Json;
+
+namespace FoodBookingAPI.Controllers
+{
+    public class UserController : ApiController
+    {
+        Dictionary<string, object> param;
+
+        [Route("api/GetUserById/{UserId}")]
+        [HttpGet]
+        public IHttpActionResult GetUserbyId(int UserId)
+        {
+            try
+            {
+                param = null;
+                param = new Dictionary<string, object>();
+                param.Add(nameof(Users.UserId), UserId);
+                DataTable result = UserRepository.GetUserById(param);
+
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+        }
+
+        [Route("api/UpdateUser")]
+        [HttpPost]
+        public IHttpActionResult UpdateUser()
+        {
+            try
+            {
+                param = null;
+                param = new Dictionary<string, object>();
+
+                string content = Request.Content.ReadAsStringAsync().Result;
+
+                string jsonContent = "[" + content + "]";
+
+                Users updatedUser = JsonConvert.DeserializeObject<List<Users>>(jsonContent)[0];
+
+                param.Add(nameof(Users.UserId), updatedUser.UserId);
+                param.Add(nameof(Users.Username), updatedUser.Username);
+                param.Add(nameof(Users.Password), updatedUser.Password);
+                param.Add(nameof(Users.FirstName), updatedUser.FirstName);
+                param.Add(nameof(Users.LastName), updatedUser.LastName);
+                param.Add(nameof(Users.Telephone), updatedUser.Telephone);
+                param.Add(nameof(Users.CreatedDate), updatedUser.CreatedDate);
+                param.Add(nameof(Users.ModifiedDate), updatedUser.ModifiedDate);
+                param.Add(nameof(Users.Logo), updatedUser.Logo);
+
+                bool success = UserRepository.UpdateUser(param);
+
+                if (success == true)
+                    return Ok();
+                return NotFound();
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+        }
+    }
+}
