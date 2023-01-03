3b769a96343e1bd94b3034eaafad8598ae71ddb2
WindyWind <19521362@gm.uit.edu.vn>
2023-01-03 21:57:20 +0700

    new model

---
+++ Frontend/Frontend/Models/CartItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Frontend.Models
+{
+    public class CartItem
+    {
+        public int CartItemId { get; set; }
+        public int SessionId { get; set; }
+        public int ProuductId { get; set; }
+        public int Quantity { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
---
+++ Frontend/Frontend/Models/ShoppingSession.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Frontend.Models
+{
+    public class ShoppingSession
+    {
+        public int SessionId { get; set; }
+        public int UserId { get; set; }
+        public int Total { get; set; }
+    }
+}
