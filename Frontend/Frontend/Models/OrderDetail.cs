25b856975b51be6ce0677e86e83547ec34e26974
WindyWind <19521362@gm.uit.edu.vn>
2023-01-04 10:43:08 +0700

    new model

---
+++ Frontend/Frontend/Models/OrderDetail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Frontend.Models
+{
+    public class OrderDetail
+    {
+        public int OrderId { get; set; }
+        public int PaymentId { get; set; }
+        public int UserId { get; set; }
+        public float Total { get; set; }
+        
+    }
+}
