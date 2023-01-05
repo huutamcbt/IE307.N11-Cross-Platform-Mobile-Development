45b36a438c6c5ce325c5bdfe8e7339889c407715
huutamcbt <huutamcbt@gmail.com>
2023-01-05 09:49:39 +0700

    Add CartItem Controller and Repository

---
+++ Backend/FoodBookingAPI/FoodBookingAPI/Controllers/CartItemController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data;
+
+using Newtonsoft.Json;
+
+using FoodBookingAPI.Models;
+using FoodBookingAPI.Repository;
+using System.Diagnostics;
+
+namespace FoodBookingAPI.Controllers
+{
+    public class CartItemController : ApiController
+    {
+        Dictionary<string, object> param;
+
+        [Route("api/GetCartItemBySessionId/{SessionId}")]
+        [HttpGet]
+        public IHttpActionResult GetCartItemBySessionId(int SessionId)
+        {
+            try
+            {
+                param = null;
+                param = new Dictionary<string, object>();
+
+                param.Add(nameof(CartItems.SessionId), SessionId);
+
+                DataTable result = CartItemRepository.GetCartItemBySessionId(param);
+                if (result != null)
+                    return Ok(result);
+                return NotFound();
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+        }
+
+        [Route("api/AddCartItem")]
+        [HttpPost]
+        public IHttpActionResult AddCartItem()
+        {
+            try
+            {
+                param = null;
+                param = new Dictionary<string, object>();
+
+                string content = Request.Content.ReadAsStringAsync().Result;
+                CartItems newCartItem = JsonConvert.DeserializeObject<CartItems>(content);
+                Debug.WriteLine(newCartItem.ProductId);
+                param.Add(nameof(CartItems.SessionId), newCartItem.SessionId);
+                param.Add(nameof(CartItems.ProductId), newCartItem.ProductId);
+                param.Add(nameof(CartItems.Quantity), newCartItem.Quantity);
+                param.Add(nameof(CartItems.CreatedDate), newCartItem.CreatedDate);
+                param.Add(nameof(CartItems.ModifiedDate), newCartItem.ModifiedDate);
+               
+                bool success = CartItemRepository.AddCartItem(param);
+                if (success == true)
+                    return Ok();
+                return NotFound();
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+        }
+
+        [Route("api/UpdateCartItem")]
+        [HttpPost]
+        public IHttpActionResult UpdateCartItem()
+        {
+            try
+            {
+                param = null;
+                param = new Dictionary<string, object>();
+                string content = Request.Content.ReadAsStringAsync().Result;
+                CartItems updatedCartItem = JsonConvert.DeserializeObject<CartItems>(content);
+
+                param.Add(nameof(CartItems.CartItemId), updatedCartItem.CartItemId);
+                param.Add(nameof(CartItems.SessionId), updatedCartItem.SessionId);
+                param.Add(nameof(CartItems.ProductId), updatedCartItem.ProductId);
+                param.Add(nameof(CartItems.Quantity), updatedCartItem.Quantity);
+                param.Add(nameof(CartItems.CreatedDate), updatedCartItem.CreatedDate);
+                param.Add(nameof(CartItems.ModifiedDate), updatedCartItem.ModifiedDate);
+
+                bool success = CartItemRepository.UpdateCartItem(param);
+
+                if (success == true)
+                    return Ok();
+                return NotFound();
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+        }
+
+        [Route("api/DeleteCartItem/{productId}/{sessionId}")]
+        [HttpDelete]
+        public IHttpActionResult DeleteCartItem(int productId, int sessionId)
+        {
+            try
+            {
+                param = null;
+                param = new Dictionary<string, object>();
+
+                param.Add(nameof(CartItems.ProductId), productId);
+                param.Add(nameof(CartItems.SessionId), sessionId);
+
+                bool success = CartItemRepository.DeleteCartItem(param);
+                if (success == true)
+                    return Ok();
+                return NotFound();
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ Backend/FoodBookingAPI/FoodBookingAPI/Repository/CartItemRepository.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+
+using FoodBookingAPI.Models;
+using System.Diagnostics;
+
+namespace FoodBookingAPI.Repository
+{
+    public class CartItemRepository
+    {
+        private static void AddParameters(SqlCommand command , Dictionary<string, object> param)
+        {
+            if(param != null)
+            {
+                foreach (KeyValuePair<string, object> data in param) 
+                {
+                    if(data.Value == null)
+                    {
+                        command.Parameters.AddWithValue("@" + data.Key, DBNull.Value);
+                    }
+                    else
+                    {
+                        switch (data.Key)
+                        {
+                            case nameof(CartItems.CartItemId):
+                                command.Parameters.Add("@" + data.Key, SqlDbType.Int);
+                                command.Parameters["@" + data.Key].Value = data.Value;
+                                Debug.WriteLine("CartItemId" + command.Parameters["@" + data.Key].Value);
+                                break;
+                            case nameof(CartItems.SessionId):
+                                command.Parameters.Add("@" + data.Key, SqlDbType.Int);
+                                command.Parameters["@" + data.Key].Value = data.Value;
+                                Debug.WriteLine("SessionId" + command.Parameters["@" + data.Key].Value);
+                                break;
+                            case nameof(CartItems.ProductId):
+                                command.Parameters.Add("@" + data.Key, SqlDbType.Int);
+                                command.Parameters["@" + data.Key].Value = data.Value;
+                                Debug.WriteLine("ProductId" + command.Parameters["@" + data.Key].Value);
+                                break;
+                            case nameof(CartItems.Quantity):
+                                command.Parameters.Add("@" + data.Key, SqlDbType.Int);
+                                command.Parameters["@" + data.Key].Value = data.Value;
+                                Debug.WriteLine("Quantity" + command.Parameters["@" + data.Key].Value);
+                                break;
+                            case nameof(CartItems.CreatedDate):
+                                command.Parameters.Add("@" + data.Key, SqlDbType.DateTime);
+                                command.Parameters["@" + data.Key].Value = data.Value;
+                                Debug.WriteLine("CreatedDate" + command.Parameters["@" + data.Key].Value);
+                                break;
+                            case nameof(CartItems.ModifiedDate):
+                                command.Parameters.Add("@" + data.Key, SqlDbType.DateTime);
+                                command.Parameters["@" + data.Key].Value = data.Value;
+                                Debug.WriteLine("ModifiedDate" + command.Parameters["@" + data.Key].Value);
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public static bool AddCartItem(Dictionary<string, object> param)
+        {
+            try
+            {
+                using(SqlConnection connection = new SqlConnection(Constant.SQLConnectionString))
+                {
+                    connection.Open();
+
+                    string query = Constant.CartItem_Procedure_AddCartItem;
+                    using(SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        AddParameters(command, param);
+                        Debug.WriteLine(param);
+                        command.CommandType = CommandType.StoredProcedure;
+                        Int32 cartItemId = Convert.ToInt32(command.ExecuteScalar());
+
+                        if (cartItemId > 0)
+                            return true;
+                        return false;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public static DataTable GetCartItemBySessionId(Dictionary<string, object> param)
+        {
+            try
+            {
+                using(SqlConnection connection = new SqlConnection(Constant.SQLConnectionString))
+                {
+                    connection.Open();
+
+                    string query = Constant.CartItem_Procedure_GetCartItemBySessionId;
+                    using(SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        AddParameters(command, param);
+
+                        using(SqlDataAdapter adapter = new SqlDataAdapter(command))
+                        {
+                            DataTable result = new DataTable();
+                            command.CommandType = CommandType.StoredProcedure;
+                            adapter.Fill(result);
+
+                            return result;
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+        public static bool UpdateCartItem(Dictionary<string, object> param)
+        {
+            try
+            {
+                using(SqlConnection connection = new SqlConnection(Constant.SQLConnectionString))
+                {
+                    connection.Open();
+
+                    string query = Constant.CartItem_Procedure_UpdateCartItem;
+                    using(SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        AddParameters(command, param);
+
+                        command.CommandType = CommandType.StoredProcedure;
+                        object result = command.ExecuteScalar();
+
+                        Int32 cartItemId = Convert.ToInt32(result);
+                        Debug.WriteLine(cartItemId);
+
+                        if (cartItemId > 0)
+                            return true;
+                        return false;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public static bool DeleteCartItem(Dictionary<string, object> param)
+        {
+            try
+            {
+                using(SqlConnection connection = new SqlConnection(Constant.SQLConnectionString))
+                {
+                    connection.Open();
+
+                    string query = Constant.CartItem_Procedure_DeleteCartItem;
+                    using(SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        AddParameters(command, param);
+
+                        command.CommandType = CommandType.StoredProcedure;
+                        int cartItemId = (int)command.ExecuteScalar();
+
+                        if (cartItemId > 0)
+                            return true;
+                        return false;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
