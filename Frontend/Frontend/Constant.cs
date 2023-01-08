7d13bb648c978b515936a6c7dda1cd76f2169404
huutamcbt <huutamcbt@gmail.com>
2023-01-09 02:14:29 +0700

    Add Constant file for store constant of MessagingCenter

---
+++ Frontend/Frontend/Constant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Frontend
+{
+    class Constant
+    {
+        public static string MessagingCenter_Title => "Title";
+        public static string MessagingCenter_Message => "Message";
+    }
+}
