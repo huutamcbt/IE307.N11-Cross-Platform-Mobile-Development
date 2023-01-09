e20dd76348eac1df56ddca8064b2ab0c5b8e3938
WindyWind <19521362@gm.uit.edu.vn>
2023-01-09 10:38:18 +0700

    blog

--- Frontend/Frontend/Views/BlogPage.xaml.cs
+++ Frontend/Frontend/Views/BlogPage.xaml.cs
@@ -1,4 +1,8 @@
 ﻿
+using Frontend.Models;
+using Frontend.Services;
+using System;
+using System.Collections.Generic;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -7,9 +11,19 @@ namespace Frontend.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class BlogPage : ContentPage
     {
+        private List<Blog> blogs = BlogService.GetBlogs();
         public BlogPage()
         {
             InitializeComponent();
+            BlogList.ItemsSource = blogs;
+        }
+
+        private async void TapBlog_Tapped(object sender, EventArgs e)
+        {
+            Frame frame = (Frame)sender;
+            TapGestureRecognizer tapGesture = (TapGestureRecognizer)frame.GestureRecognizers[0];
+            Blog blog = tapGesture.CommandParameter as Blog;
+            await Shell.Current.Navigation.PushAsync(new BlogDetail(blog));
         }
     }
 }
