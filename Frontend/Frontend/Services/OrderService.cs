8b82fc1ab173225e01c18bf7d393edd4278969a1
WindyWind <19521362@gm.uit.edu.vn>
2023-01-10 09:01:00 +0700

    fix

--- Frontend/Frontend/Services/OrderService.cs
+++ Frontend/Frontend/Services/OrderService.cs
@@ -11,17 +11,19 @@ namespace Frontend.Services
     {
         //fake orderDetail
         static List<OrderDetail> orderDetails;
-        static List<OrderItem> OrderItems;
+        static List<OrderItem> orderItems;
 
         static OrderService()
         {
             orderDetails = new List<OrderDetail>();
-            orderDetails.Add(new OrderDetail { OrderId = 1,  Total = 134654, UserId = 1 });
-            orderDetails.Add(new OrderDetail { OrderId = 1,  Total = 134654, UserId = 1 });
-            orderDetails.Add(new OrderDetail { OrderId = 1,  Total = 134654, UserId = 1 });
+            orderDetails.Add(new OrderDetail { OrderId = 1,  Total = 134654, UserId = 1,CreatedDate = DateTime.Now, ModifiedDate= DateTime.Now });
+            orderDetails.Add(new OrderDetail { OrderId = 1, Total = 134654, UserId = 1, CreatedDate = DateTime.Now, ModifiedDate = DateTime.Now });
+            orderDetails.Add(new OrderDetail { OrderId = 1, Total = 134654, UserId = 1, CreatedDate = DateTime.Now, ModifiedDate = DateTime.Now });
 
-            OrderItems = new List<OrderItem>();
-            //OrderItems.Add(new OrderItem { })
+            orderItems = new List<OrderItem>();
+            orderItems.Add(new OrderItem { OrderId = 1, OrderItemId = 1, ProductId = 1, Quantity = 3 });
+            orderItems.Add(new OrderItem { OrderId = 1, OrderItemId = 1, ProductId = 1, Quantity = 3 });
+            orderItems.Add(new OrderItem { OrderId = 1, OrderItemId = 1, ProductId = 1, Quantity = 3 });
         }
         public static async Task<HttpResponseMessage> PlaceOrder(List<OrderItem> orderItems, double Total)
         {
@@ -30,21 +32,27 @@ namespace Frontend.Services
                 ////send a request to create order
                 //OrderDetail orderDetail = new OrderDetail { Total = Total, UserId = UserService.GetUserId() };
                 //var orderStringContent = new StringContent(
-                //    JsonConvert.SerializeObject(orderDetail), 
+                //    JsonConvert.SerializeObject(orderDetail),
                 //    UnicodeEncoding.UTF8, "application/json");
                 //var firstResponse = await Base.client.PostAsync("api/CreateOrder", orderStringContent);
 
 
                 ////case can't create order
-                //if (!firstResponse.IsSuccessStatusCode) {
-                //    return firstResponse;   
+                //if (!firstResponse.IsSuccessStatusCode)
+                //{
+                //    return firstResponse;
                 //}
 
+                //String orderId = await firstResponse.Content.ReadAsStringAsync();
                 ////first reponse content return order Id
+                //foreach(OrderItem orderItem in orderItems)
+                //{
+                //    orderItem.OrderId = Int32.Parse(orderId);
+                //}
 
                 ////create orderitems
                 //var orderItemsStringContent = new StringContent(
-                //    JsonConvert.SerializeObject(orderItems), 
+                //    JsonConvert.SerializeObject(orderItems),
                 //    UnicodeEncoding.UTF8, "application/json");
                 //var secondResponse = await Base.client.PostAsync(
                 //    "api/CreateOrderItems/" + firstResponse.Content.ReadAsStringAsync(),
@@ -64,13 +72,16 @@ namespace Frontend.Services
 
 
 
-        public static async Task<List<OrderDetail>> getOrderDetail()
+        public static async Task<List<OrderDetail>> GetOrderDetail()
         {
             try
             {
-                var orderDetail = await Base.client.GetStringAsync("api/getOrderDetailByUserId/" + UserService.GetUserId());
-                List<OrderDetail> orderDetailConverted = JsonConvert.DeserializeObject<List<OrderDetail>>(orderDetail);
-                return orderDetailConverted;
+                //var orderDetail = await Base.client.GetStringAsync("api/GetOrderDetailByUserId/" + UserService.GetUserId());
+                //List<OrderDetail> orderDetailConverted = JsonConvert.DeserializeObject<List<OrderDetail>>(orderDetail);
+                //return orderDetailConverted;
+
+
+                return await Task.FromResult(orderDetails);
             }
             catch (Exception e)
             {
@@ -78,45 +89,66 @@ namespace Frontend.Services
             }
         }
 
-        public static async Task<HttpResponseMessage> RemoveItem(Product product)
+        public static async Task<List<OrderItem>> GetOrderItemsByOrderId(int orderId)
         {
             try
             {
-                //HttpResponseMessage response = await Base.client
-                //    .DeleteAsync($"api/DeleteCartItems?ProductId{product.ProductId}&sessionId{UserService.GetSessionID()}");
-                //return response;
+                //var orderItem = await Base.client.GetStringAsync("api/GetOrderItemsByOrderId/" + orderId);
+                //List<OrderItem> orderItemsConverted = JsonConvert.DeserializeObject<List<OrderItem>>(orderDetail);
+                //return orderItemsConverted;
 
 
-                return new HttpResponseMessage(System.Net.HttpStatusCode.OK);
+                return await Task.FromResult(orderItems);
             }
             catch (Exception e)
             {
                 throw e;
             }
         }
-        public static async Task<HttpResponseMessage> UpdateItem(Product product)
-        {
-            try
-            {
-                //CartItem cartItem = new CartItem
-                //{
-                //    SessionId = UserService.GetSessionID(),
-                //    ProuductId = product.ProductId,
-                //    Quantity = product.Quantity,
-                //    ModifiedDate = new DateTime()
-                //};
-                //var json = JsonConvert.SerializeObject(cartItem);
-                //var stringContent = new StringContent(json, UnicodeEncoding.UTF8, "application/json"); // use MediaTypeNames.Application.Json in Core 3.0+ and Standard 2.1+
-                //var response = await Base.client.PostAsync("api/AddToCart", stringContent);
-                //return response;
 
 
-                return new HttpResponseMessage(System.Net.HttpStatusCode.OK);
-            }
-            catch (Exception e)
-            {
-                throw e;
-            }
-        }
+
+
+        // unuse
+        //public static async Task<HttpResponseMessage> RemoveItem(Product product)
+        //{
+        //    try
+        //    {
+        //        //HttpResponseMessage response = await Base.client
+        //        //    .DeleteAsync($"api/DeleteCartItems?ProductId{product.ProductId}&sessionId{UserService.GetSessionID()}");
+        //        //return response;
+
+        //        await Task.FromResult(1);
+        //        return new HttpResponseMessage(System.Net.HttpStatusCode.OK);
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        throw e;
+        //    }
+        //}
+        //public static async Task<HttpResponseMessage> UpdateItem(Product product)
+        //{
+        //    try
+        //    {
+        //        //CartItem cartItem = new CartItem
+        //        //{
+        //        //    SessionId = UserService.GetSessionID(),
+        //        //    ProuductId = product.ProductId,
+        //        //    Quantity = product.Quantity,
+        //        //    ModifiedDate = new DateTime()
+        //        //};
+        //        //var json = JsonConvert.SerializeObject(cartItem);
+        //        //var stringContent = new StringContent(json, UnicodeEncoding.UTF8, "application/json"); // use MediaTypeNames.Application.Json in Core 3.0+ and Standard 2.1+
+        //        //var response = await Base.client.PostAsync("api/AddToCart", stringContent);
+        //        //return response;
+
+        //        await Task.FromResult(1);
+        //        return new HttpResponseMessage(System.Net.HttpStatusCode.OK);
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        throw e;
+        //    }
+        //}
     }
 }
