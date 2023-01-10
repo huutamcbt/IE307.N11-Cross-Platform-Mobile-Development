e1e3956cf49393d9125880ef533a6150e7081fff
WindyWind <19521362@gm.uit.edu.vn>
2023-01-10 10:43:04 +0700

    order histỏy

---
+++ Frontend/Frontend/ViewModels/OrderHistoryViewModel.cs
@@ -0,0 +1,51 @@
+﻿using Frontend.Models;
+using Frontend.Services;
+using Frontend.Views;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Frontend.ViewModels
+{
+    class OrderHistoryViewModel: INotifyPropertyChanged
+    {
+        private IList<OrderDetail> sourse;
+        public ObservableCollection<OrderDetail> orderList { get; private set; }
+        public ICommand OrderTapCommand { get; private set; }
+        public OrderHistoryViewModel()
+        {
+            sourse = new List<OrderDetail>();
+            OrderTapCommand = new Command<OrderDetail>(async (orderDetail) => {
+                await Shell.Current.GoToAsync($"{nameof(OrderDetailPage)}?OrderId={orderDetail.OrderId}");
+            });
+            Task.Run(async () =>
+            {
+                await InitializeOrders();
+            }).Wait();
+        }
+        async Task InitializeOrders()
+        {
+            List < OrderDetail >  orders= await OrderService.GetOrderDetail();
+            foreach(OrderDetail order in orders)
+            {
+                sourse.Add(order);
+            }
+            orderList = new ObservableCollection<OrderDetail>(sourse);
+            OnPropertyChanged("orderList");
+        }
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
