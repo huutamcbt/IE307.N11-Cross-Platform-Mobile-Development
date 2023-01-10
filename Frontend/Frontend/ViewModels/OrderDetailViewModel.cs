d7d983c49f7766b94d116ea6d1b7609f360df969
WindyWind <19521362@gm.uit.edu.vn>
2023-01-10 10:48:18 +0700

    order detail

---
+++ Frontend/Frontend/ViewModels/OrderDetailViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Frontend.ViewModels
+{
+    [QueryProperty("OrderId", "OrderId")]
+    class OrderDetailViewModel: INotifyPropertyChanged
+    {
+        private int orderId;
+        public int OrderId
+        {
+            get => orderId;
+            set
+            {
+                orderId = value;
+                //initializeProduct(value);
+            }
+        }
+
+
+
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
